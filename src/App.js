9f0cc460a39a72e5388a0642ac25e1b01fde2276
Henri Koskinen <hkoskinen@protonmail.com>
2018-05-05 21:24:50 +0300

    Add TodoItem component and few sample items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const TodoItem = ({text}) => (
+  <div className="todo-item">{text}</div>
+);
+
+const TodoItems = ({items}) => (
+  <div className="todo-items">
+    {items.length ? (
+      items.length && items.map(item => <TodoItem text={item.text} />)
+    ) : (
+      <div>You don't have any todo items.</div>
+    )}
+  </div>
+);
+
 class App extends Component {
+  state = {
+    todoItems: [
+      { text: 'Remember the milk' },
+      { text: 'Find the meaning of life' },
+      { text: 'Binge watch Mr. Robot' }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>TODO APP</h1>
+        <TodoItems items={this.state.todoItems} />
       </div>
     );
   }
