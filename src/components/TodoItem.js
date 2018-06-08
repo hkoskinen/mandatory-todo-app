a07f4d77a2cf8a095fb6c74fd3563c5af7b91c9c
Henri Koskinen <hkoskinen@protonmail.com>
2018-06-08 16:12:28 +0300

    Add item styles

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './TodoItem.css';
 
 const TodoItem = ({id, text, handleRemoveItem}) => {
   return (
     <div className="todo-item">
-      <button onClick={()=>handleRemoveItem(id)}>X</button> {text}
+      <p>{text}</p>
+      <button onClick={()=>handleRemoveItem(id)}>X</button>
     </div>
   );
 };
