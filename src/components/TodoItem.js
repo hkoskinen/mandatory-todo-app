db298580b14280ae598ec9dccb076455b6346f7f
Henri Koskinen <hkoskinen@protonmail.com>
2018-06-09 09:23:28 +0300

    Add Font Awesome and Normalize.css

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,7 +5,7 @@ const TodoItem = ({id, text, handleRemoveItem}) => {
   return (
     <div className="todo-item">
       <p>{text}</p>
-      <button onClick={()=>handleRemoveItem(id)} className="button">+</button>
+      <button onClick={()=>handleRemoveItem(id)} className="button"><i class="fa fa-trash-alt"></i></button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './normalize-v8.0.0.css';
 import './index.css';
+
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
